d28678298e4c21c47d0c47e5694fe978259c0206
jimin6826 <jiminbag521@gmail.com>
2025-09-08 15:39:41 +0900

    if

---
+++ 조건문 반복문 1.py
@@ -0,0 +1,15 @@
+numbers = []
+total = 0
+
+while True:
+    num = int(input("숫자를 입력하시오: "))
+    numbers.append(num)
+    total += num
+
+    if total > 100:
+        break
+
+
+print("입력된 숫자들:", numbers)
+print("숫자의 개수:", len(numbers))
+print("숫자의 합", total)
